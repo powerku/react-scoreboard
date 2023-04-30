78f44bbae65b445e1bd67bae723b03191347291e
구영준 <powerku@naver.com>
2023-04-30 16:02:31 +0900

    update : 소리 기능 추가 단축키 추가
    -단축키 추가
    spaceBar : 공격시간 시작/중지
    enter : 전체시간 시작 중지

--- src/components/ScorePointButton.js
+++ src/components/ScorePointButton.js
@@ -1,7 +1,62 @@
 import classes from "./ScorePointButton.module.css";
+import { useCallback, useEffect } from "react";
 function ScorePointButton(props) {
   let score = props.score;
   const setScore = props.setScore;
+  const handleKeyUp = useCallback(
+    (event) => {
+      // do stuff with stateVariable and event
+      console.log(event);
+    },
+    [score]
+  );
+
+  useEffect(() => {
+    const shortcut = (event) => {
+      console.log(event.key);
+      const key = event.key.toUpperCase();
+      if (props.type === "Home") {
+        if (key === "F") {
+          event.preventDefault();
+          plus1point();
+        }
+        if (key === "D") {
+          event.preventDefault();
+          plus2point();
+        }
+        if (key === "S") {
+          event.preventDefault();
+          plus3point();
+        }
+        if (key === "A") {
+          event.preventDefault();
+          minus1point();
+        }
+      } else {
+        if (key === "J") {
+          event.preventDefault();
+          plus1point();
+        }
+        if (key === "K") {
+          event.preventDefault();
+          plus2point();
+        }
+        if (key === "L") {
+          event.preventDefault();
+          plus3point();
+        }
+        if (key === ";") {
+          event.preventDefault();
+          minus1point();
+        }
+      }
+    };
+    document.addEventListener("keyup", shortcut);
+    return () => {
+      document.removeEventListener("keyup", shortcut);
+    };
+  }, [handleKeyUp]);
+
   function minus1point() {
     score = score - 1;
     if (score < 0) {
